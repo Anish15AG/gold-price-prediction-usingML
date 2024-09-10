10a7ecffbbee3209719143f7c290ec65239baa0b
Anish <anish.agclg@gmail.com>
2024-09-11 02:12:46 +0530

    App.py reference file: Second Commit

--- app.py
+++ app.py
@@ -11,3 +11,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 
+
+#Load the DataSet
+st.title("Gold Prediction Application")
+
+#Upload the DataSet
+uploaded_file = st.file_uploader('/content/drive/MyDrive/GoldPricePrediction_dataset/gld_price_data.csv', type="csv")
+
+if uploaded_file is not None:
+  gold_data = pd.read_csv(uploaded_file)
+
+  #Display the DataSet
+  st.write("DataSet Preview: ")
+  st.DataFrame(gold_data.head())
+
+  #Data Cleaning and Processing
+  gold_data = gold_data.apply(pd.to_numeric, errors='coerce')
+
+  #Creation of Feature Set
+  X = gold_data.drop(['Date', 'GLD'], axis=1)
+  Y = gold_data['GLD']
+
+  
