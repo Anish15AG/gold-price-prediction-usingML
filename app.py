f25f01f5139c2d74020e466eae22d2c1c44695ae
Anish <anish.agclg@gmail.com>
2024-09-11 02:11:12 +0530

    App.py reference file: initial commit with package imports

---
+++ app.py
@@ -0,0 +1,13 @@
+'''
+This file is only for reference to host it on Streamlit/LocalHost
+'''
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+
